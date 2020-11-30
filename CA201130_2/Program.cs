9851dd034df9b1d6e487b10f26008e1ba5ffb132
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-11-30 10:38:49 +0100

    Add project files.

---
+++ CA201130_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA201130_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var conn = new SqlConnection(
+                @"Server=(localdb)\MSSQLLocalDB;" +
+                "Database=proba");
+
+            conn.Open();
+
+            var cmd = new SqlCommand("SELECT Year(szul) AS szulEv FROM emberek", conn);
+            SqlDataReader r = cmd.ExecuteReader();
+
+            while (r.Read())
+            {
+                Console.WriteLine(r["szulEv"]);
+            }
+
+
+            conn.Close();
+            Console.ReadKey(true);
+        }
+    }
+}
